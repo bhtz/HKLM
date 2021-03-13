aa529a22e97d4ed5197baf8041be8089360e00bf
bheintz <benjamin.heintz@soprasterianext.com>
2021-03-13 19:40:57 +0100

    preparing analytic & remoteconfig frontend

---
+++ src/Microscope.Admin/Pages/Analytic/Analytic.razor.cs
@@ -0,0 +1,39 @@
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Blazored.Toast.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.JSInterop;
+
+namespace Microscope.Admin.Pages.Analytic
+{
+    public partial class Analytic : ComponentBase
+    {
+        #region injected properties
+
+        [Inject]
+        private IAccessTokenProvider TokenProvider { get; set; }
+        [Inject]
+        private HttpClient Http { get; set; }
+        [Inject]
+        private IJSRuntime JsRuntime { get; set; }
+        [Inject]
+        private IToastService ToastService { get; set; }
+        #endregion
+
+        protected override async Task OnInitializedAsync()
+        {
+            await this.SetHttpHeaders();
+        }
+
+        private async Task SetHttpHeaders()
+        {
+            var accessTokenResult = await TokenProvider.RequestAccessToken();
+            if (accessTokenResult.TryGetToken(out var token))
+            {
+                Http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Value);
+            }
+        }
+    }
+}
