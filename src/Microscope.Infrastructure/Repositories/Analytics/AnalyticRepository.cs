c3fc23d52818ba3e7a12ce4153efab5991e1baed
tomadj <thomas.adjadj@outlook.com>
2021-05-09 00:07:58 +0200

    EF Repository Implementations

---
+++ src/Microscope.Infrastructure/Repositories/Analytics/AnalyticRepository.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microscope.Domain.Aggregates.AnalyticAggregate;
+using Microscope.Domain.Entities;
+using Microscope.Domain.SharedKernel;
+
+namespace Microscope.Infrastructure.Repositories.Analytics
+{
+    public class AnalyticRepository : EFRepositoryBase<Analytic>, IAnalyticRepository
+    {
+
+
+        public AnalyticRepository(MicroscopeDbContext context) : base(context)
+        {
+        }
+
+       
+    }
+}
